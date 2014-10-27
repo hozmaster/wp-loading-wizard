cfe0025433d40683d7aafd4cb569afa01d728a17
Olli-Pekka Wallin <olli-pekka.wallin@hawinaproductions.com>
2014-10-27 14:04:01 +0200

    Add assests folder and default php file

---
+++ ubuntudownloadwizard.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name:  Loadingwizard
+Plugin URI: http://chartbeat.com/wordpress/
+Description: ADds laoding wizard for Ubuntu Finland home page.
+Version: 0.0.1
+Author: Ubuntu fi
+Author URI: http://ubuntu-fi.org/
+*/
