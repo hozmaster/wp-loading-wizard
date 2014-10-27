f5dda35c9a562344308b0b66ebf139e3038816c1
Olli-Pekka Wallin <olli-pekka.wallin@hawinaproductions.com>
2014-10-27 14:20:10 +0200

    remove foo add some images. Some other changes too

--- ubuntudownloadwizard.php
+++ ubuntudownloadwizard.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name:  Ubunutdownloadwizard
+Plugin Name:  Ubuntudownloadwizard
 Plugin URI: http://ubuntu.com/wordpress/whatever
-Description: Add Ubunut Download wizard for Ubuntu Finland home page.
+Description: Add Ubuntu Download wizard for Ubuntu Finland home page.
 Version: 0.0.1
 Author: Ubuntu fi
 Author URI: http://ubuntu-fi.org/
